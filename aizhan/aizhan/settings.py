cd49f77782621c222d1705888e0869cc4a12a203
zxl <zhao1532072926@gmail.com>
2018-05-21 15:48:45 +0800

    爱站网sitesInfo爬虫有一部分网站被反爬了，怀疑是没有设置user_agent的原因，设置一下user_agent，同时禁止使用cookies

--- aizhan/aizhan/settings.py
+++ aizhan/aizhan/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'aizhan.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'aizhan (+http://www.yourdomain.com)'
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 "
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -33,7 +33,7 @@ ROBOTSTXT_OBEY = True
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
